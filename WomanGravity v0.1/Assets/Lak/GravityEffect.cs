d51bbf53fe62cc8b20d946e366701e9f69c39163
Lakhvir <lakhvir.martin@enti.cat>
2019-10-23 12:41:06 +0200

    v.0.2
    Verison Lak

---
+++ WomanGravity v0.1/Assets/Lak/GravityEffect.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityEffect : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    private Move player;
+
+    private bool top;
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        player = GetComponent<Move>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            rb.gravityScale *= -1;
+            Rotation();
+        }
+    }
+
+    void Rotation()
+    {
+        if (top == false)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 180f);
+        }
+        else
+        {
+            transform.eulerAngles = Vector3.zero;
+        }
+        //player.movimiento = !player.movingLeft;
+        top = !top;
+    }
+}
