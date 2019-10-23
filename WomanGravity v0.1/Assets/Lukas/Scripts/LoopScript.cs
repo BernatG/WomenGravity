48876bba9853cd20e22ee29dcd07d6e8b1fbb755
Cibaly <lukasbetoni@gmail.com>
2019-10-23 12:53:54 +0200

    v.0.3
    Lukas

---
+++ WomanGravity v0.1/Assets/Lukas/Scripts/LoopScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoopScript : MonoBehaviour
+{
+
+    public GameObject Player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // temporary reset condition for checking the loop works
+        if (Player.GetComponent<Transform>().position.y < -3)
+        {
+            Debug.Log("Try to loop");
+            Loop();
+        }
+    }
+
+    // Resets the current scene
+    void Loop()
+    {
+        Debug.Log("Looped");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
+
+
